7ad6c80be42dadd80068cb56ff3ad3124f9ae1a2
IwonaJarkiewicz <politea@gmail.com>
2017-08-20 14:52:22 +0200

    kliknięcie na 2 link

--- js/events.js
+++ js/events.js
@@ -3,4 +3,19 @@
 function klikHeader() {
     console.log('kliknąłeś w header');
 }
+
+function alarm(event) {
+    event.preventDefault(); //zapobiegamy domyślnej akcji
+    console.log('kliknięto w link');
+    console.log(event.type);
+}
+
+var secondLink = document.getElementsByTagName('a')[1];
+secondLink.onclick = alarm;
+
 document.getElementById('title-header').onclick = klikHeader; //wywołaj funkcję na elemencie o id 'title-header'
+
+var paragraf = document.getElementsByTagName('p')[0];
+
+//paragraf.addEventListener('click', klikHeader);
+document.getElementsByTagName('p')[0].addEventListener('click', klikHeader)
